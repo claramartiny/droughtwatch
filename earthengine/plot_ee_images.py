295a854ddac2caa3ec89f5bab5f5619c2ec71b38
Clara Martiny <clara.msy@gmail.com>
2020-12-07 16:25:23 +0100

    modified functions to get 1) 3 bands for plot and 2) 11 bands for model

--- earthengine/plot_ee_images.py
+++ earthengine/plot_ee_images.py
@@ -17,17 +17,23 @@ def parse_visual(data):
     iterator = dataset.make_one_shot_iterator()
 
     features = {
+        'B1': tf.FixedLenSequenceFeature([65], tf.int64,allow_missing=True),
         'B2': tf.FixedLenSequenceFeature([65], tf.int64,allow_missing=True),
         'B3': tf.FixedLenSequenceFeature([65], tf.int64,allow_missing=True),
         'B4': tf.FixedLenSequenceFeature([65], tf.int64,allow_missing=True),
-        'B5': tf.FixedLenSequenceFeature([65], tf.int64,allow_missing=True)
-
+        'B5': tf.FixedLenSequenceFeature([65], tf.int64,allow_missing=True),
+        'B6': tf.FixedLenSequenceFeature([65], tf.int64,allow_missing=True),
+        'B7': tf.FixedLenSequenceFeature([65], tf.int64,allow_missing=True),
+        'B8': tf.FixedLenSequenceFeature([65], tf.int64,allow_missing=True),
+        'B9': tf.FixedLenSequenceFeature([65], tf.int64,allow_missing=True),
+        'B10': tf.FixedLenSequenceFeature([65], tf.int64,allow_missing=True),
+        'B11': tf.FixedLenSequenceFeature([65], tf.int64,allow_missing=True)
     }
 
     parsed_examples = [tf.parse_single_example(data, features) for data in iterator]
     return parsed_examples
 
-def get_img_from_example(parsed_example, intensify=True):
+def get_rgb_img_to_plot(parsed_example, intensify=True):
     rgbArray = np.zeros((65,65,3), 'int64')
     for i, band in enumerate(['B5', 'B3', 'B2']):
         band_data = parsed_example[band].numpy()
@@ -37,6 +43,17 @@ def get_img_from_example(parsed_example, intensify=True):
             band_data = band_data*255
         rgbArray[..., i] = band_data
     return rgbArray
+
+def get_X_test_all_bands(parsed_example, intensify=True):
+    elevenArray = np.zeros((65,65,11), 'int64')
+    for i, band in enumerate(['B1','B4', 'B3', 'B2','B5','B6','B7','B8','B9','B10','B11']):
+        band_data = parsed_example[band].numpy()
+        if intensify:
+            band_data = band_data/np.max(band_data)*255
+        else:
+            band_data = band_data*255
+        elevenArray[..., i] = band_data
+    return elevenArray
 
 def main(i):
     parsed_examples = parse_visual(training_files[i])
