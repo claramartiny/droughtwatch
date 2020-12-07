6212e0a36d273c2271f6eb5a1912cc646085d189
camil belmaachi <camilbel16@gmail.com>
2020-12-07 14:19:46 +0100

    changes to streamlit

--- api/app.py
+++ api/app.py
@@ -4,11 +4,13 @@ from PIL import Image
 import torch
 
 # Display text on a browser
-st.write("# Testing streamlit")
+st.title("Drought Watch")
+st.header("Drought detection using satelite images")
+st.text("Upload a satelite Image for image classification as drought detection:")
 
 #CODE to upload a file
-
 upload_file = st.file_uploader("Choose an image file", type = ["jpg", "png"])
+
 if upload_file is not None:
     img = Image.open(upload_file).convert("RGB")
     imageLocation = st.empty()
@@ -30,4 +32,39 @@ if upload_file is not None:
 @st.cache
 def post_process(outputs, nms_thres=0.3):
     boxes = outputs['boxes'].data
+
+
+
+# import cv2
+# from PIL import Image, ImageOps
+# import numpy as np
+# def import_and_predict(image_data, model):
+
+#         size = (150,150)
+#         image = ImageOps.fit(image_data, size, Image.ANTIALIAS)
+#         image = np.asarray(image)
+#         img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+#         img_resize = (cv2.resize(img, dsize=(75, 75),    interpolation=cv2.INTER_CUBIC))/255.
+
+#         img_reshape = img_resize[np.newaxis,...]
+
+#         prediction = model.predict(img_reshape)
+
+#         return prediction
+# if file is None:
+#     st.text("Please upload an image file")
+# else:
+#     image = Image.open(file)
+#     st.image(image, use_column_width=True)
+#     prediction = import_and_predict(image, model)
+
+#     if np.argmax(prediction) == 0:
+#         st.write("It is a paper!")
+#     elif np.argmax(prediction) == 1:
+#         st.write("It is a rock!")
+#     else:
+#         st.write("It is a scissor!")
+
+#     st.text("Probability (0: Drought, 1: Rock, 2: Scissor")
+#     st.write(prediction)
 
