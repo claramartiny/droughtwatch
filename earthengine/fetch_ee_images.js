f21e863c48951b40fb799c2b21c3c9a61f4839de
Tom Sachs <tom.sachsp@gmail.com>
2020-12-08 09:57:30 +0100

    fixed .js

--- earthengine/fetch_ee_images.js
+++ earthengine/fetch_ee_images.js
@@ -1,33 +1,36 @@
 // copy code below in https://code.earthengine.google.com/
 
-var StartDate = '2020-10-12';
+var StartDate = '2017-09-15';
 var EndDate = '2020-12-03';
 
 // Fetch region and rectangle from cropping_coordinates.py
-var region = '[[0.186205,36.288562],[0.177622,36.288562], [0.186205, 36.315341],[0.177622, 36.315341]]';
-var rectangle = [0.186205,36.315341,0.177622,36.288562];
 
-
-var rectangle1 = ee.Geometry.Rectangle(rectangle);
+var rectangle1 = ee.Geometry.Rectangle([-68.5085,0.0128,-68.4906,0.0307]);
 
 var selectors = ['B1','B2','B3','B4','B5','B6','B7','B8','B9','B10','B11']
 
 var dataset = ee.ImageCollection('LANDSAT/LC08/C01/T1_RT')
                   .filterBounds(rectangle1)
                   .filterDate(StartDate, EndDate)
+                  .sort('CLOUD_COVER')
                   .select(selectors)
                   .first();
+
 
 print(selectors)
 
 var image_export_options = {
-  'patchDimensions': [65, 65]}
-  
-Export.image.toDrive({
+  'patchDimensions': [65, 65]};
+
+ Export.image.toDrive({
   image:dataset,
-  description: 'Testearthengine1',
+  region:rectangle1,
+  description: 'Amazonia5',
   dimensions:'65x65',
-  scale:30,
   fileFormat:'TFRecord',
   formatOptions: image_export_options,
 })
+
+Map.centerObject(rectangle1)
+Map.addLayer(dataset,{bands:['B4','B3','B2'],min:0,max:13000}, 'rgb')
+Map.addLayer(rectangle1,{color:'red'},'testregion')
